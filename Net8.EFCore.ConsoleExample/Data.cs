2429f736842d9280bf0d9426618a81ba7dd963e9
Felaray Ho <felaray@gmail.com>
2023-12-17 12:50:09 +0800

    加入專案檔案。

---
+++ Net8.EFCore.ConsoleExample/Data.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class BloggingContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Blog> Blogs { get; set; }
+    public DbSet<Post> Posts { get; set; }
+
+    public BloggingContext()
+    {
+    }
+
+    //
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlServer("Data Source=.;Initial Catalog=ConsoleExampleDB;Integrated Security=True;Trust Server Certificate=True");
+}
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public List<Blog> Blogs { get; } = new();
+}
+
+public class Blog
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public List<Post> Posts { get; } = new();
+}
+
+public class Post
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+}
---
+++ Net8.EFCore.ConsoleExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using var db = new BloggingContext();
+
+//Add User
+Console.WriteLine("Add User");
+var user = new User { Name = "User1" };
+db.Users.Add(user);
+await db.SaveChangesAsync();
+Console.WriteLine(user.Id);
+
+
+//Get User
+var result = await db.Users.FindAsync(user.Id);
+Console.WriteLine(result.Name);
+
+//Update User
+Console.WriteLine("Update User");
+result.Name = "User2";
+await db.SaveChangesAsync();
+var result2 = await db.Users.FindAsync(user.Id);
+Console.WriteLine(result2.Name);
+
+
+
+//Delete User
+Console.WriteLine("Delete User");
+db.Users.Remove(result);
+await db.SaveChangesAsync();
+var result3 = await db.Users.FindAsync(user.Id);
+Console.WriteLine(result3?.Name ?? "null");
